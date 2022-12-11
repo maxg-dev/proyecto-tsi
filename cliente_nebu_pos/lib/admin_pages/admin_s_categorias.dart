cc82728397041eec7b625e7da2721a86992da4dd
maxg-dev <maximiliano.galvez@usm.cl>
2022-12-11 14:31:05 -0300

    se termina mantenedor categoria servicios

--- cliente_nebu_pos/lib/admin_pages/admin_s_categorias.dart
+++ cliente_nebu_pos/lib/admin_pages/admin_s_categorias.dart
@@ -1,7 +1,11 @@
+import 'package:cliente_nebu_pos/admin_pages/s_categoria_pages/s_categoria_add_page.dart';
+import 'package:cliente_nebu_pos/admin_pages/s_categoria_pages/s_categoria_update_page.dart';
 import 'package:flutter/material.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
-
+import '../services/p_categorias_provider.dart';
 import '../services/s_categorias_provider.dart';
+import 'p_categoria_pages/p_categoria_add_page.dart';
+import 'p_categoria_pages/p_categoria_update_page.dart';
 
 class AdminSCategorias extends StatefulWidget {
   const AdminSCategorias({super.key});
@@ -13,29 +17,80 @@ class AdminSCategorias extends StatefulWidget {
 class _AdminSCategoriasState extends State<AdminSCategorias> {
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: SCategoriasProvider().getAll(),
-      builder: (context, AsyncSnapshot snapshot) {
-        if (!snapshot.hasData) {
-          return Center(child: CircularProgressIndicator());
-        }
-        return Expanded(
-          child: Padding(
-            padding: const EdgeInsets.only(top: 15),
-            child: ListView.separated(
-              separatorBuilder: (context, index) => Divider(),
-              itemCount: snapshot.data.length,
-              itemBuilder: (context, index) {
-                var categoria = snapshot.data[index];
-                return ListTile(
-                  leading: Icon(MdiIcons.cookie),
-                  title: Text('Producto: ${categoria['nombre']}'),
+    return Expanded(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Expanded(
+            child: FutureBuilder(
+              future: SCategoriasProvider().getAll(),
+              builder: (context, AsyncSnapshot snapshot) {
+                if (!snapshot.hasData) {
+                  return Center(child: CircularProgressIndicator());
+                }
+                return Padding(
+                  padding: const EdgeInsets.only(top: 15),
+                  child: ListView.separated(
+                    separatorBuilder: (context, index) => Divider(),
+                    itemCount: snapshot.data.length,
+                    itemBuilder: (context, index) {
+                      var categoria = snapshot.data[index];
+                      return ListTile(
+                        leading: Icon(MdiIcons.dropbox),
+                        title: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                                'Nombre: ${categoria['nombre'].toString().padRight(20)}'),
+                            barraBotones(categoria),
+                          ],
+                        ),
+                      );
+                    },
+                  ),
                 );
               },
             ),
           ),
-        );
-      },
+          Padding(
+            padding: const EdgeInsets.all(5),
+            child: SizedBox(
+              width: 170,
+              child: ElevatedButton(
+                  style: ButtonStyle(),
+                  onPressed: () => pageSwap(context, SCategoriaAddPage()),
+                  child: Row(
+                      children: [Icon(Icons.add), Text('Agregar categoria')])),
+            ),
+          ),
+        ],
+      ),
     );
+  }
+
+  Row barraBotones(objeto) {
+    return Row(children: [
+      // Boton editar
+      Padding(
+        padding: const EdgeInsets.only(right: 5),
+        child: ElevatedButton(
+            child: Icon(Icons.edit),
+            onPressed: () =>
+                pageSwap(context, SCategoriaUpdatePage(objeto['id']))),
+      ),
+      // Boton eliminar
+      ElevatedButton(
+          child: Icon(MdiIcons.delete),
+          onPressed: () async =>
+              SCategoriasProvider().delete(objeto['id']).then(
+                    (value) => setState(() {}),
+                  ))
+    ]);
+  }
+
+  pageSwap(BuildContext context, Widget page) {
+    Navigator.push(context, MaterialPageRoute(builder: (context) => page))
+        .then((value) => setState(() {}));
   }
 }
--- cliente_nebu_pos/lib/services/s_categorias_provider.dart
+++ cliente_nebu_pos/lib/services/s_categorias_provider.dart
@@ -4,8 +4,48 @@ import 'package:http/http.dart' as http;
 
 class SCategoriasProvider {
   final apiURL = 'http://127.0.0.1:8000/api';
+  //
   Future<List<dynamic>> getAll() async {
     var respond = await http.get(Uri.parse(apiURL + '/servicioCategorias'));
     return respond.statusCode == 200 ? json.decode(respond.body) : [];
+  }
+
+  Future<Map<String, dynamic>> get(int id) async {
+    var respond = await http
+        .get(Uri.parse(apiURL + '/servicioCategorias/' + id.toString()));
+    return json.decode(respond.body);
+  }
+
+  Future<Map<String, dynamic>> add(String nombre) async {
+    var respond = await http.post(Uri.parse(apiURL + '/servicioCategorias'),
+        headers: <String, String>{
+          'Content-type': 'application/json',
+          'Accept': 'application/json'
+        },
+        body: jsonEncode(<String, dynamic>{
+          'nombre': nombre,
+        }));
+
+    return json.decode(respond.body);
+  }
+
+  Future<Map<String, dynamic>> update(int id, String nombre) async {
+    var respond = await http.put(
+        Uri.parse(apiURL + '/servicioCategorias/' + id.toString()),
+        headers: <String, String>{
+          'Content-type': 'application/json',
+          'Accept': 'application/json'
+        },
+        body: jsonEncode(<String, dynamic>{
+          'nombre': nombre,
+        }));
+
+    return json.decode(respond.body);
+  }
+
+  Future<bool> delete(int id) async {
+    var respond = await http
+        .delete(Uri.parse(apiURL + '/servicioCategorias/' + id.toString()));
+    return respond.statusCode == 200;
   }
 }
